bf36696cd45ebb96154083752d5efb49bca80b67
kaiorosa1 <kaiosrosa@gmail.com>
2019-12-31 21:46:51 -0300

    add tachyons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Navigation from './Navigation';
+import Navigation from './components/Navigation';
 import Logo from './Logo';
 import ImageLinkForm from './ImageLinkForm';
 import FaceRecognition from './FaceRecognition';
 
 import './App.css';
 
-function App() {
+const  App = () =>{
   return (
     <div className="App">
       <Navigation />
---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Navigation.css'
+
+const Navigation =() =>{
+    return (  
+        <nav>
+            <p>Sign out</p>
+        </nav>   
+    );
+}
+
+
+export default Navigation;

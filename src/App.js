26791c450975d877693764623d7814b9541d8ae8
kaiorosa1 <kaiosrosa@gmail.com>
2020-01-01 23:35:11 -0300

    add Rank component and changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './FaceRecognition';
 
@@ -11,6 +12,7 @@ const  App = () =>{
     <div className="App">
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       <FaceRecognition />
     </div>
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,12 +1,16 @@
 import React from 'react';
-
+import './ImageLinkForm.css'
 
 const ImageLinkForm =() =>{
     return (
         <div>
-            <p className="f3">{'This is the face recognition app'}</p>
-            <input type="text" />
-            <button>Recognize</button>
+            <p className="f3">{'Magic brain will detect faces in pictures. Give it a try!'}</p>
+            <div className="center">
+                <div className="form center pa4 br5 shadow-5">
+                    <input className="f4 pa2 w-70 center" type="text" />
+                    <button className="w-30 grow f4 link ph3 pv2 dib white bg-dark-blue" >Detect</button>
+                </div>
+            </div>
         </div>
     );
 }
